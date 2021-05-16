41ab789b9bc8468673a4790a3e1569dabae5837d
duynv <duynv@mobio.vn>
2021-05-16 21:56:45 +0700

    Cover about topic: dynamic component rendering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExampleContainerComponent } from './example-container/example-container.component';
+import { DynamicContentOneComponent } from './dynamic-content-one/dynamic-content-one.component';
+import { DynamicContentTwoComponent } from './dynamic-content-two/dynamic-content-two.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExampleContainerComponent,
+    DynamicContentOneComponent,
+    DynamicContentTwoComponent
   ],
   imports: [
     BrowserModule,
